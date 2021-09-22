ba5710932660ce995cdb0e202e37ea189dfacba7
Josh Carlson <jdcarls2@gmail.com>
2021-09-22 09:27:37 -0500

    merge all routes, not just roads

--- imposm/midwest/generalized_layers.sql
+++ imposm/midwest/generalized_layers.sql
@@ -1,13 +1,14 @@
--- Recreate generalized layers
+-- Recreate generalized and merged layers
 
--- Road routes, generalized 50 m
+-- Routes, merged by ref/network
 
-DROP TABLE IF EXISTS midwest_roadroutes_gen50;
+DROP TABLE IF EXISTS midwest_mergedroutes_gen50;
 
-CREATE TABLE midwest_roadroutes_gen50 (
+CREATE TABLE midwest_mergedroutes_gen50 (
 	id SERIAL PRIMARY KEY,
 	ref VARCHAR,
-	network VARCHAR
+	network VARCHAR,
+	route_type VARCHAR,
 );
 
 SELECT AddGeometryColumn('public'::varchar,
@@ -20,36 +21,77 @@ SELECT AddGeometryColumn('public'::varchar,
 BEGIN;
 
 WITH a AS(
-	SELECT   osm_id,
-	         ref,
-		     network,
-	         st_simplifypreservetopology(st_collect(geometry), 50) AS geometry
+	SELECT   ref,
+			 network,
+			 type,
+			 st_simplifypreservetopology(st_collect(geometry), 50) AS geometry
 	FROM     midwest_routes
-	WHERE    type = 'road'
-	GROUP BY osm_id, network, ref
+	WHERE    ref <> '' and network <> ''
+	GROUP BY network, ref, type
 )
 
-INSERT INTO midwest_roadroutes_gen50 (ref, network, geometry)
-SELECT ref, network, geometry FROM a;
+INSERT INTO midwest_mergedroutes_gen50 (ref, network, route_type, geometry)
+SELECT ref, network, type, geometry FROM a;
 
 COMMIT;
 
-CREATE INDEX sidx_midwest_roadroutes_gen50
-  ON midwest_roadroutes_gen50
+CREATE INDEX sidx_midwest_mergedroutes_gen50
+  ON midwest_mergedroutes_gen50
   USING GIST (geometry);
 
--- Road routes, generalized by 200 m
 
-DROP TABLE IF EXISTS midwest_roadroutes_gen200;
+-- Routes, generalized by 50m
 
-CREATE TABLE midwest_roadroutes_gen200 (
+DROP TABLE IF EXISTS midwest_mergedroutes_gen50;
+
+CREATE TABLE midwest_mergedroutes_gen50 (
 	id SERIAL PRIMARY KEY,
 	ref VARCHAR,
-	network VARCHAR
+	network VARCHAR,
+	route_type VARCHAR,
 );
 
 SELECT AddGeometryColumn('public'::varchar,
-						 'midwest_roadroutes_gen200'::varchar,
+						 'midwest_roadroutes_gen50'::varchar,
+						 'geometry'::varchar,
+						 3857,
+						 'GEOMETRY'::varchar,
+						 2);
+
+BEGIN;
+
+WITH a AS(
+	SELECT   ref,
+			 network,
+			 type,
+			 st_simplifypreservetopology(st_collect(geometry), 50) AS geometry
+	FROM     midwest_routes
+	WHERE    ref <> '' and network <> ''
+	GROUP BY network, ref, type
+)
+
+INSERT INTO midwest_mergedroutes_gen50 (ref, network, route_type, geometry)
+SELECT ref, network, type, geometry FROM a;
+
+COMMIT;
+
+CREATE INDEX sidx_midwest_mergedroutes_gen50
+  ON midwest_mergedroutes_gen50
+  USING GIST (geometry);
+
+-- Routes, generalized by 200 m
+
+DROP TABLE IF EXISTS midwest_mergedroutes_gen200;
+
+CREATE TABLE midwest_mergedroutes_gen200 (
+	id SERIAL PRIMARY KEY,
+	ref VARCHAR,
+	network VARCHAR,
+	route_type VARCHAR
+);
+
+SELECT AddGeometryColumn('public'::varchar,
+						 'midwest_mergedroutes_gen200'::varchar,
 						 'geometry'::varchar,
 						 3857,
 						 'GEOMETRY'::varchar,
@@ -59,22 +101,22 @@ SELECT AddGeometryColumn('public'::varchar,
 BEGIN;
 
 WITH a AS(
-	SELECT   osm_id,
-	         ref,
-		     network,
-	         st_simplifypreservetopology(st_collect(geometry), 200) AS geometry
+	SELECT   ref,
+			 network,
+			 type,
+			 st_simplifypreservetopology(st_collect(geometry), 200) AS geometry
 	FROM     midwest_routes
-	WHERE    type = 'road'
-	GROUP BY osm_id, network, ref
+	WHERE    ref <> '' and network <> ''
+	GROUP BY network, ref, type
 )
 
-INSERT INTO midwest_roadroutes_gen200 (ref, network, geometry)
-SELECT ref, network, geometry FROM a;
+INSERT INTO midwest_mergedroutes_gen200 (ref, network, route_type, geometry)
+SELECT ref, network, type, geometry FROM a;
 
 COMMIT;
 
-CREATE INDEX sidx_midwest_roadroutes_gen200
-  ON midwest_roadroutes_gen200
+CREATE INDEX sidx_midwest_mergedroutes_gen200
+  ON midwest_mergedroutes_gen200
   USING GIST (geometry);
   
 -- Water areas, generalized by 200m, excluding waterways for small scale
