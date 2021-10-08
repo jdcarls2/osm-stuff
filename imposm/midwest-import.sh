ac709a007f0112e304b4e00992a2756f65dd4164
Josh Carlson <jdcarls2@gmail.com>
2021-10-08 10:38:51 -0500

    fold import/preimport together

--- imposm/midwest-import.sh
+++ imposm/midwest-import.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# remove prior import dirs
+cd /home/joshua/osm-stuff/imposm/midwest
+rm -r ./imposm_diff
+rm -r ./imposm_cache
+# import new data
 cd /home/joshua/osm-stuff/imposm/imposm-0.11.1-linux-x86-64
 ./imposm import -config ../midwest/config.json -read ../midwest/data/us-midwest-latest.osm.pbf -write -diff -diffdir ../midwest/imposm_diff -optimize -deployproduction
--- imposm/midwest-preimport.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/joshua/osm-stuff/imposm/midwest
-rm -r ./imposm_diff
-rm -r ./imposm_cache
-rm -r ./data
