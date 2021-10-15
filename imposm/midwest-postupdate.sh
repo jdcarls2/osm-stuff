80e4572859d1c28f4ec2a28e26c2a1841699f6c3
Josh Carlson <jdcarls2@gmail.com>
2021-10-14 21:36:18 -0500

    update to reflect name change on sql file

--- imposm/midwest-postupdate.sh
+++ imposm/midwest-postupdate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-psql -h localhost -d osm -U osm -p 5432 -a -q -f /home/joshua/osm-stuff/imposm/midwest/generalized_layers.sql
+psql -h localhost -d osm -U osm -p 5432 -a -q -f /home/joshua/osm-stuff/imposm/midwest/post_update.sql
