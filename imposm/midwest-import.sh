e08dc7b0e403d219823241c9891bb4a7acad0a74
Josh Carlson <jdcarls2@gmail.com>
2021-10-26 07:39:00 -0500

    include wget in import w/ timestamp

--- imposm/midwest-import.sh
+++ imposm/midwest-import.sh
@@ -3,6 +3,9 @@
 cd /home/joshua/osm-stuff/imposm/midwest
 rm -r ./imposm_diff
 rm -r ./imposm_cache
+# get new pbf file
+cd /home/joshua/osm-stuff/imposm/midwest/data
+wget -N https://download.geofabrik.de/north-america/us-midwest-latest.osm.pbf
 # import new data
 cd /home/joshua/osm-stuff/imposm/imposm-0.11.1-linux-x86-64
 ./imposm import -config ../midwest/config.json -read ../midwest/data/us-midwest-latest.osm.pbf -write -diff -diffdir ../midwest/imposm_diff -optimize -deployproduction
