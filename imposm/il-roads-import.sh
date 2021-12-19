90489798e308cb6ee302075612b6940575aba35f
Josh Carlson <jdcarls2@gmail.com>
2021-12-18 19:13:24 -0600

    il roads

--- imposm/il-roads-import.sh
+++ imposm/il-roads-import.sh
@@ -8,4 +8,4 @@ cd /home/joshua/osm-stuff/imposm/il-roads/data
 wget -N https://download.geofabrik.de/north-america/us/illinois-latest.osm.pbf
 # import new data
 cd /home/joshua/osm-stuff/imposm/imposm-0.11.1-linux-x86-64
-./imposm import -config ../il-roads/config.json -read ../il-roads/data/us-illinois-latest.osm.pbf -write -diff -diffdir ../il-roads/imposm_diff -optimize -deployproduction
+./imposm import -config ../il-roads/config.json -read ../il-roads/data/illinois-latest.osm.pbf -write -diff -diffdir ../il-roads/imposm_diff -optimize -deployproduction
