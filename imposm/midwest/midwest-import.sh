1267b55e08c6e5c9517cbcd908ab3890062acc37
Joshua Carlson <jdcarls2@gmail.com>
2022-06-26 21:54:26 -0500

    get the /data dir back, grab state file

--- imposm/midwest/midwest-import.sh
+++ imposm/midwest/midwest-import.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # remove prior import dirs
-cd /home/joshua/osm-stuff/imposm/midwest
 rm -r ./imposm_diff
 rm -r ./imposm_cache
 # get new pbf file
-cd /home/joshua/osm-stuff/imposm/midwest/data
+cd ./data
 wget -N https://download.geofabrik.de/north-america/us-midwest-latest.osm.pbf
+wget -N https://download.geofabrik.de/north-america/us-midwest-updates/state.txt
 # import new data
-cd ../imposm
-./imposm import -config ../midwest/config.json -read ../midwest/us-midwest-latest.osm.pbf -write -diff -diffdir ../midwest/imposm_diff -optimize -deployproduction
+cd ../../imposm
+./imposm import -config ../midwest/config.json -read ../midwest/data/us-midwest-latest.osm.pbf -write -diff -diffdir ../midwest/imposm_diff -optimize -deployproduction
