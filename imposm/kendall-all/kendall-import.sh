44ba4a4ec0a68692b65b5283268c56a3d42d4956
Josh Carlson <jdcarls2@gmail.com>
2022-09-17 21:52:39 -0500

    create data dir

--- imposm/kendall-all/kendall-import.sh
+++ imposm/kendall-all/kendall-import.sh
@@ -3,6 +3,7 @@
 rm -r ./imposm_diff
 rm -r ./imposm_cache
 # get new pbf file
+mkdir data
 cd ./data
 wget -N https://download.geofabrik.de/north-america/us/illinois-latest.osm.pbf
 # import new data
